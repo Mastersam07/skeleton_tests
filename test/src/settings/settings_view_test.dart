e0f936b37faa66d72fc7a4f2c1901e6d062cc889
Mastersam07 <abadasamuelosp@gmail.com>
2023-12-01 19:51:24 +0100

    :white_check_mark: chore: tests for settings view

---
+++ test/src/settings/settings_view_test.dart
@@ -0,0 +1,42 @@
+import 'package:coinpay/src/settings/settings_controller.dart';
+import 'package:coinpay/src/settings/settings_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../mocks.dart';
+
+void main() {
+  testWidgets('$SettingsView Dropdown updates theme mode',
+      (WidgetTester tester) async {
+    // Arrange
+    final mockService = MockSettingsService();
+    final mockController = SettingsController(mockService);
+
+    when(() => mockService.themeMode())
+        .thenAnswer((_) => Future.value(ThemeMode.system));
+    when(() => mockService.updateThemeMode(ThemeMode.light))
+        .thenAnswer((_) => Future.value());
+
+    await mockController.loadSettings();
+    await tester.pumpWidget(MaterialApp(
+      home: SettingsView(controller: mockController),
+    ));
+
+    await mockController.loadSettings();
+
+    // Initial state check
+    expect(mockController.themeMode,
+        equals(ThemeMode.system)); // Assuming this is the default
+
+    // Act: Open dropdown and select 'Light Theme'
+    await tester.tap(find.byType(DropdownButton<ThemeMode>));
+    await tester.pumpAndSettle(); // Wait for the dropdown to open
+
+    await tester.tap(find.text('Light Theme').last);
+    await tester.pumpAndSettle(); // Wait for the dropdown to close
+
+    // Assert
+    expect(mockController.themeMode, equals(ThemeMode.light));
+  });
+}
