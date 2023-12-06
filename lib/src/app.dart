cff43cdfed1c9ba09f7982c5615a87cfa3aa2450
jinyus <starboymc4@gmail.com>
2023-12-06 06:38:26 -0500

    migrate to state_beacon

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:state_beacon/state_beacon.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
@@ -58,7 +59,7 @@ class MyApp extends StatelessWidget {
           // SettingsController to display the correct theme.
           theme: ThemeData(),
           darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
+          themeMode: settingsController.themeMode.watch(context),
 
           // Define a function to handle named routes in order to support
           // Flutter web url navigation and deep linking.
